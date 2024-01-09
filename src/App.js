cffcfa93875d318fda35ba44d44a6391b8dbdf9f
C1Banks <Bankscur@gmail.com>
2024-01-08 21:58:25 -0800

    css styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        
+        <div className ="header">React App</div>
+        
       
-      <div>React App</div>
+
+      <div className = "projectOneContainer">Project #1</div>
+        <div className = "projectOneDescript"align = "left">ProjectOneDescription</div>
+      <div className = "projectTwoContainer">Project #2</div>
+        <div className="projectTwoDescript"align = "right">ProjectTwoDescription</div>
     </div>
-    
   );
 }
 
