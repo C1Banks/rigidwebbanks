d4d9149a670f7e88b2db95b634bb41cd3605060e
C1Banks <Bankscur@gmail.com>
2024-01-12 11:56:47 -0800

    css styling and animations

--- src/animations/index.js
+++ src/animations/index.js
@@ -1,11 +1,13 @@
 import {motion} from "framer-motion"
 import React from "react"
 import "../App.css"
+//import dottedBackGround from './images/dottedBackGround.png';
+
 
 
 function PopAnimate ({children}) {
     return(
-        <motion.div className = "skillsContainer" 
+        <motion.div className = "skillsContainer"
         initial="hidden"
         whileInView="visible"
         viewport={{once: true}}
@@ -19,10 +21,12 @@ function PopAnimate ({children}) {
           </motion.div>
         
         )}
+
 function SlideAnimate ({children}){
     return(
         <div className='introBox'>
-        <motion.div className="introBoxContent"
+          
+          <motion.div className="introBoxContent"
                 initial={{ y:400 }}
                 animate= {{y:0}}
                 //whileInView={{ }}
@@ -31,9 +35,49 @@ function SlideAnimate ({children}){
                     {children}
 
             </motion.div>
+            
         </div>
     )
 }
 
+function ProjectOnePopAnimate ({children}){
+    return(
+        <motion.div className = "projectOneContainer"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{once: true}}
+        transition={{duration: 1.2}}
+        variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 }
+          }}
+          >
+            {children}
+          </motion.div>
+    )
+}
+function ProjectTwoPopAnimate ({children}){
+    return(
+        <motion.div className = "projectTwoContainer"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{once: true}}
+        transition={{duration: 1.2}}
+        variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 }
+          }}
+          >
+            {children}
+          </motion.div>
+    )
+}
+
+
+
+
+
 export {PopAnimate};   
 export {SlideAnimate};
+export {ProjectOnePopAnimate};
+export {ProjectTwoPopAnimate};
